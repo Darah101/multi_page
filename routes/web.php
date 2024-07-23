bc13d99b8127ea943eadbee9520f209687dc0b73
Darah101 <darahbacus@gmail.com>
2024-07-23 09:09:27 +0100

    adding pages

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,32 @@ Route::get('name/{name?}', function ($name= 'Guest'){
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+Route::get('/pricing', function () {
+    return view('pricing');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/bloghome', function () {
+    return view('bloghome');
+});
+
+Route::get('/portfolio-overview', function () {
+    return view('portfolio-overview');
+});
+
+Route::get('/portfolio-item', function () {
+    return view('portfolio-item');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
