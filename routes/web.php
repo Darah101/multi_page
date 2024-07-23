aebd50f59fd1631f28e93ff65e9b5e0683ba4db2
Darah101 <darahbacus@gmail.com>
2024-07-23 06:11:56 +0100

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/product', function(){
+    return view('product', ['name' =>'shoes']);
+});
+
+Route::get('/multi_page', function(){
+    return view ('multi_page');
+});
+
+Route::get('name/{name?}', function ($name= 'Guest'){
+    return('My name is '.$name);
+});
+
